e1520868f9e39101d002b4196eabde953af6456e
Aman Kumar <95639657+amankumar0369@users.noreply.github.com>
2024-12-15 19:04:20 +0000

    data type

---
+++ 01_basic/test.js
@@ -0,0 +1,25 @@
+"use strict";   // treat all js code as newer version
+
+// alert( 8 +9)  // we are using nodejs, not browser
+
+console.log(3+3);
+
+let name ="hitesh";
+let age= 18;
+let isLoggedIn = false;
+
+// number
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined => value not asign
+// symbol => unique
+
+// object
+
+console.log(typeof "aman");
+
+console.log(typeof undefined);
+
+console.log(typeof "null"); //iska type object hota hai  but undefine mai undefine
