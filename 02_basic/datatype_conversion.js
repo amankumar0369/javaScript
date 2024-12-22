967b763950508bb4ccedb8b846f2654c991f53c4
Aman Kumar <95639657+amankumar0369@users.noreply.github.com>
2024-12-22 14:15:49 +0000

    datatype_conversion

---
+++ 02_basic/datatype_conversion.js
@@ -0,0 +1,29 @@
+let score =303;
+//  two way to find a datatype 
+// 1. Normal way.
+console.log(typeof score);
+// 2. Second is method way .
+console.log(typeof(score));
+//  how to change a datatype.
+let valueInString = String(score);
+console.log(typeof valueInString);
+console.log(valueInString);
+
+// New
+let test = "biology";
+console.log(typeof test);
+console.log(typeof(test));
+let changeNumber = Number(test);
+console.log(typeof changeNumber);
+console.log(changeNumber);
+
+// new
+let nu = "892";
+console.log(typeof(nu));
+let changeNum = Number(nu);
+console.log(typeof changeNum);
+console.log(changeNum);
+
+
+// 99 => 99
+// string => number => nun
